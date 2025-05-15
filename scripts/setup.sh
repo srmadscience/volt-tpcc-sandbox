9690f2ac8393aed3d893921d49f959b0554408c5
dwrolfe <david.rolfe@pobix.com>
2025-05-15 10:05:32 +0100

    fixed bash issue

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,30 +10,4 @@
 
 . $HOME/.profile
 
-#VENV=`which voltenv`
-#. ${VENV} 
-. /home/ubuntu/voltdb-ent-14.2.0-x86_64/bin/voltenv
-APPNAME="tpcc"
-
-cd
-
-if 
-	[ ! -d logs ]
-then
-	mkdir logs
-fi
-
-cd volt-tpcc-sandbox
-cd tpcc
-
-# compile java source
-javac -classpath $APPCLASSPATH src/com/procedures/*.java client/com/*.java
-# build procedure and client jars
-jar cf $APPNAME-procs.jar -C client com/Constants.class -C src com/procedures
-jar cf $APPNAME-client.jar -C client com
-# remove compiled .class files
-rm -rf src/com/procedures/*.class client/com/*.class
-
-sqlcmd --servers=vdb1 < ddl.sql
-
-exit 0
+sudo bash /home/ubuntu/volt-tpcc-sandbox/scripts/setup.bash
